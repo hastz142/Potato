9223d7364a5c5abd587e920da3d97ff9e210c962
hastz142 <160739659+hastz142@users.noreply.github.com>
2024-09-25 18:01:27 -0300

    Update faleconosco.php

--- faleconosco.php
+++ faleconosco.php
@@ -1,10 +1,14 @@
-<?php
-echo "Fale Conosco<br>";
-echo "Preencha o formulário abaixo para entrar em contato:<br>";
-echo "<form action='envia_contato.php' method='POST'>";
-echo "Nome: <input type='text' name='nome'><br><br>";
-echo "Email: <input type='email' name='email'><br><br>";
-echo "Mensagem:<br><textarea name='mensagem'></textarea><br><br>";
-echo "<input type='submit' value='Enviar'><br>";
-echo "</form>";
-?>
+<h1>Fale Conosco</h1>
+<p>Preencha o formulário abaixo para entrar em contato:</p>
+<form action="envia_contato.php" method="POST">
+    <label for="nome">Nome:</label><br>
+    <input type="text" id="nome" name="nome" required><br><br>
+
+    <label for="email">Email:</label><br>
+    <input type="email" id="email" name="email" required><br><br>
+
+    <label for="mensagem">Mensagem:</label><br>
+    <textarea id="mensagem" name="mensagem" required></textarea><br><br>
+
+    <input type="submit" value="Enviar">
+</form>
