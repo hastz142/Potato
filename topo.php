22829245de7bd8e6c7e82960a2d9bcdc4a6fd1cc
hastz142 <160739659+hastz142@users.noreply.github.com>
2024-09-25 18:41:53 -0300

    Update topo.php

--- topo.php
+++ topo.php
@@ -1,11 +1,7 @@
-<?php
-echo "<link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Silkscreen&display=swap'>";
-echo "<link rel='stylesheet' type='text/css' href='estilo.css'>";
-?>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Silkscreen&display=swap">
+<link rel="stylesheet" type="text/css" href="estilo.css">
 
 <div>
-    <h1>
-        Bem vindo(a) ao meu site. <br>
-        aqui é onde tudo começa.
-    </h1>
+    <h1>Bem-vindo(a) ao meu site.</h1>
+    <p>Aqui é onde tudo começa.</p>
 </div>
