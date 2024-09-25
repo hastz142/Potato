70fa18611a3772f1ba78c8c0fc92e513243b35ed
hastz142 <160739659+hastz142@users.noreply.github.com>
2024-09-25 18:39:42 -0300

    Update rodape.php

---
+++ rodape.php
@@ -0,0 +1,16 @@
+<footer>
+    <p>&copy; 2024 MeuSite.com. Todos os direitos reservados.</p>
+    <p>Contato: contato@meusite.com | Telefone: (11) 1234-5678</p>
+    <p>
+        <a href="index.php">Home</a> | 
+        <a href="index.php?pg=quemsomos">Quem Somos</a> | 
+        <a href="index.php?pg=clientes">Clientes</a> | 
+        <a href="index.php?pg=faleconosco">Fale Conosco</a>
+    </p>
+    <p>
+        Siga-nos nas redes sociais: 
+        <a href="#">Facebook</a> | 
+        <a href="#">Twitter</a> | 
+        <a href="#">Instagram</a>
+    </p>
+</footer>
