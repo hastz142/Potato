d6d65faac2f54644e8bf55812d43d632c93bc4ba
hastz142 <160739659+hastz142@users.noreply.github.com>
2024-09-25 22:02:05 -0300

    Update principal.php

--- principal.php
+++ principal.php
@@ -1,2 +1,23 @@
-<h1>Página Principal</h1>
-<p>Bem-vindo à página principal do MeuSite.com. Aqui você encontrará as informações mais relevantes.</p>
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Silkscreen&display=swap">
+</head>
+<body>
+<h1 class="titulo">Site desenvolvido com o intuito de relaxar...</h1>
+<h3 class="titulo">Bem-vindo à página principal. Aqui você será introduzido ao tema.</h3>
+
+<div class="container">
+  <div class="parte">
+    <h2>Parte 1</h2>
+      <p>
+  Vamos conhecer um pouco sobre Hollow Knigth? <br>
+  Hollow Knight é um jogo de ação e aventura desenvolvido pela Team Cherry. Lançado em 2017, ele se destaca por sua arte em 2D, atmosfera envolvente e jogabilidade desafiadora.
+      </p>
+  </div>
+  
+  <div class="parte">
+    <h2>Parte 2</h2>  
+      <p>
+  O jogo apresenta um estilo de arte desenhado à mão, com ambientes sombrios e detalhados que criam uma atmosfera única.
+  Conta com uma 
+      </p>
+</body>
